ee2ce9aacc1cf2cc80a73f87f2cb1a072c0b5db2
Denis <gundenis1@gmail>
2024-08-19 11:46:25 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // Breakpoints
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const swiper1 = new Swiper('.work-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,    
+    spaceBetween: 10,
+    // Breakpoints
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+  window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
